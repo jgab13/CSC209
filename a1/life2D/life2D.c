f3ed5b430bd0919feb29fece33fe4107220d6f4c
Gabe Jonathan <gabejona@teach.cs.toronto.edu>
2020-01-23 17:43:21 -0500

    Finally it worksgit add life2D.c life2D_helpers.cgit add life2D.c life2D_helpers.cgit add life2D.c life2D_helpers.cgit add life2D.c life2D_helpers.cgit add life2D.c life2D_helpers.cgit add life2D.c life2D_helpers.cgit add life2D.c life2D_helpers.c

--- a1/life2D/life2D.c
+++ a1/life2D/life2D.c
@@ -10,6 +10,30 @@ int main(int argc, char **argv) {
         fprintf(stderr, "Usage: life2D rows cols states\n");
         return 1;
     }
+    int rows = strtol(argv[1], NULL, 10);
+    int cols = strtol(argv[2], NULL, 10);
+    int board[rows * cols];
+
+    int states = strtol(argv[3], NULL, 10);
+    FILE * data = stdin;
+       
+    int index = 0; 
+    int value;
+    while (fscanf(data, "%d", &value) == 1){
+	board[index] = value;
+	index++;
+    }
+    
+    for (int j = 0; j < states; j++){
+        print_state(board, rows, cols);
+ 	update_state(board, rows, cols);
+    }
+    
+    if (fclose(data) != 0){
+	fprintf(stderr, "fclose failed\n");
+	return 1;
+    }
+    
 
     // TODO: Implement.
 }
--- a1/life2D/life2D_helpers.c
+++ a1/life2D/life2D_helpers.c
@@ -12,7 +12,62 @@ void print_state(int *board, int num_rows, int num_cols) {
     printf("\n");
 }
 
+int count_neighbors(int *board, int i, int col){
+    int count =0;
+    if (board[i+1] == 1){
+	count++;
+    }
+    if (board[i-1] == 1){
+	count++;
+    }
+    if (board[i+col] == 1){
+	count++;
+    }
+    if (board[i+col+1] == 1){
+	count++;
+    }
+    if (board[i+col-1] == 1){
+	count++;
+    }
+    if (board[i-col] == 1){
+	count++;
+    }
+    if (board[i-col-1] == 1){
+	count++;
+    }
+    if (board[i-col+1] == 1){
+	count++;
+    }
+    return count;
+}
+
 void update_state(int *board, int num_rows, int num_cols) {
+    int copy[num_rows * num_cols];
+    for (int i = 0; i < num_rows * num_cols; i++){
+	copy[i] = board[i];
+    }
+    for (int i = 0; i < num_rows * num_cols; i++){
+	if ((i % num_cols != 0) && (i % num_cols != num_cols - 1) && (i > num_cols - 1) && (i < (num_cols * (num_rows - 1)))){
+	    int count = count_neighbors(copy, i, num_cols);
+	    if (count == 2 || count == 3){
+		if (copy[i] == 0){
+		    board[i] = 1;
+		}
+	    } else if (count < 2 || count > 3){
+		if (copy[i] == 1){
+		    board[i] = 0;
+		}
+	    }	
+	}
+    }
     // TODO: Implement.
     return;
 }
+
+
+
+
+
+
+
+
